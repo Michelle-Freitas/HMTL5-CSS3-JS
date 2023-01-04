54c3b22d4b4500019efa609bd76abacbedda1083
Michelle-Freitas <michelle.afreitas7@gmail.com>
2023-01-04 13:15:04 -0300

    contador functions

--- meus_projetos/contador-cronometro/javascript/index.js
+++ meus_projetos/contador-cronometro/javascript/index.js
@@ -1,8 +1,49 @@
+const contador = document.querySelector('#contador')
+let btnResetar = document.querySelector('#btnResetar')
+let btnPausar = document.querySelector('#btnPausar')
+let segundos = 60
+let horas = 0
+
 function Iniciar(){
-    let valorContagem = document.querySelector('input[name="opcao"]:checked').value
+    let valorContagem = Number.parseInt(document.querySelector('input[name="opcao"]:checked').value)
     console.log(valorContagem)
 
     HabilitarPausarReset()
+    HabilitarInicio()
+    DesabilitarInicio()
+
+    contagem = setInterval(function(){
+        if (segundos > 0) {
+            segundos-= 1
+        } else {
+            segundos = 59
+        }
+        contador.innerHTML = ` ${0} : ${valorContagem-1} : ${segundos} `
+    }, 1000)
+}
+
+
+function FinalizarContagem(){
+    contador.innerHTML = ` 00 : 00 : 00 `
+    contador.innerHTML += '<p id="esgotado">Tempo Esgotado!</p>'
+}
+
+
+function Resetar(){
+    document.querySelector('form').reset();
+    clearInterval(contagem)
+    minutos = 0
+    segundos = 0
+    valorContagem = 0
+    contador.innerHTML = '00 : 00 : 00'
+    DesabilitarInicio()
+    btnPausar.disabled = true
+    btnResetar.disabled = true
+}
+
+
+function Pausar(){
+    clearInterval(contagem)
     HabilitarInicio()
 
 }
@@ -11,23 +52,27 @@ function Iniciar(){
 function Escolher(){
     let valorContagemEsc = document.querySelector('#escolhaDigitada').value
     console.log(valorContagemEsc)
-
 }
+
 
 function HabilitarInicio(){
     if (document.querySelector('#btnIniciar').disabled == true) {
         document.querySelector('#btnIniciar').disabled = false
-    } else {
+    }
+}
+
+function DesabilitarInicio(){
+    if (document.querySelector('#btnIniciar').disabled == false) {
         document.querySelector('#btnIniciar').disabled = true
     }
 }
 
 function HabilitarPausarReset(){
-    if (document.querySelector('#btnPausar').disabled == true) {
-        document.querySelector('#btnPausar').disabled = false
+    if (btnPausar.disabled == true) {
+        btnPausar.disabled = false
     }
 
-    if (document.querySelector('#btnResetar').disabled == true) {
-        document.querySelector('#btnResetar').disabled = false
+    if (btnResetar.disabled == true) {
+        btnResetar.disabled = false
     }
 }
